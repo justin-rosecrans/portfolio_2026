847747a404fa41f2eee013bc1b1a77b3632850f5
Justin Rosecrans <jrscrns@gmail.com>
2026-04-02 19:15:15 -0500

    update to text

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -15,7 +15,7 @@ const About: React.FC = () => {
               Justin has obtained the following certifications: (1) Google AI Professional Certificate, 2026, (2) AWS Certified Cloud Practitioner, 2025 and (3) Advanced React from Meta, 2023.
             </p>
             <p>
-              Justin has experience in building, developing and deploying to production web applications that improve usability and enhance performance for the company. He has a strong background in a variety of full stack web technologies with a focus on Front End. Justin is passionate about the application of new technologies in Artificial Intelligence (AI) and implementing the emerging technologies into web applications.
+              Justin has experience in building, developing and deploying to production web applications that improve usability and enhance performance for the company. He has a strong background in a variety of web technologies with a focus on Front End. Justin is passionate about the application of new technologies in Artificial Intelligence (AI) and implementing the emerging technologies into web applications.
             </p>
           </Col>
         </Row>
